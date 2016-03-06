b46d7dc338c876c188da871338507f816c9f19c5
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-03-06 12:13:08 -0500

    Create Movement class.

---
+++ InteractiveFiction/src/character/Movement.java
@@ -0,0 +1,19 @@
+package character;
+
+import rooms.CompassRoom;
+
+public class Movement {
+	CompassRoom currentRoom;
+	public Movement(CompassRoom room) {
+		this.currentRoom = room;
+	}
+	
+	public void move(String direction) {
+			currentRoom = currentRoom.getExitRoom(direction); 
+	}
+	
+	public CompassRoom getCurrentRoom() {
+		return currentRoom;
+	}
+	
+}
