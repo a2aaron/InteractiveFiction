f2a3958fcd9920f79887ca62124c542e85925eed
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-04-02 12:35:00 -0400

    Create GenericRoom class.

---
+++ InteractiveFiction/src/rooms/GenericRoom.java
@@ -0,0 +1,23 @@
+package rooms;
+
+public abstract class GenericRoom {
+	String roomName;
+	String roomDescription;
+	
+	public GenericRoom() {
+		// TODO Auto-generated constructor stub
+	} 
+	
+	public abstract void addExit(Enum exit);
+	public abstract void linkExitTo(GenericRoom room);
+	
+	public String getRoomName() { return roomName;}
+	public String getRoomDescription() { return roomDescription; }
+	public abstract Enum getExits();
+	public abstract Enum getEntrances();
+
+	public void setRoomName(String roomName) { this.roomName = roomName; }
+	public void setRoomDescription(String roomDescription) { this.roomDescription = roomDescription; }
+	public abstract Enum setExits();
+	public abstract Enum setEntrances();
+}
